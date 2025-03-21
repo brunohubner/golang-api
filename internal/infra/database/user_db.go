cb6d38db8bb2f600a81c9e62720171e34d090c70
Bruno Hubner <brunohubnerdev@gmail.com>
2025-03-20 23:26:18 -0300

    feat: Add sqlite3 and gorm sqlite driver dependencies

--- internal/infra/database/user_db.go
+++ internal/infra/database/user_db.go
@@ -1,6 +1,9 @@
 package database
 
-import "gorm.io/gorm"
+import (
+	"github.com/brunohubner/golang-api/internal/entity"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	DB *gorm.DB
@@ -8,4 +11,14 @@ type User struct {
 
 func NewUser(db *gorm.DB) *User {
 	return &User{DB: db}
+}
+
+func (u *User) Create(user *entity.User) error {
+	return u.DB.Create(user).Error
+}
+
+func (u *User) FindByEmail(email string) (*entity.User, error) {
+	user := &entity.User{}
+	err := u.DB.Where("email = ?", email).First(user).Error
+	return user, err
 }
---
+++ internal/infra/database/user_db_test.go
@@ -0,0 +1,42 @@
+package database_test
+
+import (
+	"testing"
+	// "github.com/brunohubner/
+
+	"github.com/brunohubner/golang-api/internal/entity"
+	"github.com/brunohubner/golang-api/internal/infra/database"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var (
+	fakeUserName  = "John Doe"
+	fakeUserEmail = "john@doe.com"
+	fakeUserPass  = "@Pass123"
+)
+
+func TestCreateUser(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	db.AutoMigrate(&entity.User{})
+
+	user, _ := entity.NewUser(fakeUserName, fakeUserEmail, fakeUserPass)
+	userDB := database.NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound := entity.User{}
+	db.First(&userFound, user.ID)
+
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.Equal(t, user.Password, userFound.Password)
+}
