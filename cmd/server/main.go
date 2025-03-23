4c6903c21165f7109be98e2dd3c28e46ccfdcfad
Bruno Hubner <brunohubnerdev@gmail.com>
2025-03-23 17:00:45 -0300

    feat: Add product creation functionality

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,67 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"net/http"
 
 	"github.com/brunohubner/golang-api/configs"
+	"github.com/brunohubner/golang-api/internal/dto"
+	"github.com/brunohubner/golang-api/internal/entity"
+	"github.com/brunohubner/golang-api/internal/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	// path do env file uma pasta atrás
-	config, _ := configs.LoadConfig("../")
+	_, err := configs.LoadConfig("../")
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Println(config.DbDriver)
-	fmt.Println(config.DbHost)
-	fmt.Println(config.DbPort)
+	db, err := gorm.Open(sqlite.Open("sqlite.test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.User{}, &entity.Product{})
+
+	productDB := database.NewProduct(db)
+	productHandler := NeProductHandler(productDB)
+
+	http.HandleFunc("/api/v1/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":8001", nil)
+}
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NeProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
