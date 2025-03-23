045e16bb70bb34679a806fae6930951e2467ac68
Bruno Hubner <brunohubnerdev@gmail.com>
2025-03-23 15:13:37 -0300

    feat: Add ProductInterface and Product database implementation

--- internal/infra/database/user_db_test.go
+++ internal/infra/database/user_db_test.go
@@ -2,7 +2,6 @@ package database_test
 
 import (
 	"testing"
-	// "github.com/brunohubner/
 
 	"github.com/brunohubner/golang-api/internal/entity"
 	"github.com/brunohubner/golang-api/internal/infra/database"
@@ -35,6 +34,30 @@ func TestCreateUser(t *testing.T) {
 	userFound := entity.User{}
 	err = db.First(&userFound, "id = ?", user.ID).Error
 
+	assert.Nil(t, err)
+
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.Equal(t, user.Password, userFound.Password)
+}
+
+func TestFindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	db.AutoMigrate(&entity.User{})
+
+	user, _ := entity.NewUser(fakeUserName, fakeUserEmail, fakeUserPass)
+	userDB := database.NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(fakeUserEmail)
 	assert.Nil(t, err)
 
 	assert.Equal(t, user.ID, userFound.ID)
